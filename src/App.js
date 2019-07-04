e62b2be10ae6b83ecf9710ae9c8cb0a220111003
Will Worth <will.worth@gmail.com>
2019-07-04 10:29:01 +0200

    add color palette

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+import Palette from "./Palette";
+import SeedPalettes from "./seedColors";
 
 function App() {
   return (
-    <div className="App">
-      <h1>color app</h1>
+    <div>
+      <Palette palette={SeedPalettes[4]} />
     </div>
   );
 }
