f3725ec8714a06e3314512b293a0dac133e0a213
Will Worth <will.worth@gmail.com>
2019-07-06 14:50:15 +0200

    add colorhelper

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Palette from "./Palette";
 import SeedPalettes from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(SeedPalettes[4]));
+
   return (
     <div>
       <Palette {...SeedPalettes[2]} />
