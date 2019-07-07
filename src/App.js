a341c6cae94fbb0cfa2dd15a55ab66bf406c717e
Will Worth <will.worth@gmail.com>
2019-07-07 07:02:09 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import SeedPalettes from "./seedColors";
 import { generatePalette } from "./colorHelpers";
@@ -7,9 +8,14 @@ function App() {
   console.log();
 
   return (
-    <div>
-      <Palette palette={generatePalette(SeedPalettes[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+      <Route exact path="/palette/:id" render={() => <h1>indiv palette </h1>} />
+    </Switch>
+
+    // <div>
+    //   <Palette palette={generatePalette(SeedPalettes[4])} />
+    // </div>
   );
 }
 
